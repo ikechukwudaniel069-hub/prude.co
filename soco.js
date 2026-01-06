53fe8b2a03e04e653549b89750f17c6acf12c707
ikechukwudaniel069-hub <ikechukwudaniel069@gmail.com>
2026-01-06 08:51:49 -0800

    Add files via upload

--- soco.js
+++ soco.js
@@ -3,6 +3,7 @@ let htmlCONTENT=document.querySelector('#html-content');
 let csScontent=document.querySelector('#css-content');
 let jscontent=document.querySelector('#js-content');
 let cssbtn=document.querySelector('#cssbtn');
+let about=document.querySelector("#about");
 
 
 
@@ -11,18 +12,28 @@ let htmlbtn=document.querySelector('#htmlbtn');
 htmlbtn.onclick=function(){
     maincontent.style.display="none";
     htmlCONTENT.style.display="block";
-    csScontent.style.display="none";
-    jscontent.style.display="none";
+    csScontent.style.display="";
+    jsContent.style.display="none";
+    about.style.display="none";
 } 
 cssbtn.onclick=function(){
     maincontent.style.display="none";
     csScontent.style.display="block";
     jscontent.style.display="none";
     htmlCONTENT.style.display="none";
+ about.style.display="none";
+}
+if(cssbtn.onclick=="true"){
+        csScontent.style.display="block";
+ htmlCONTENT.style.display="none";
+  about.style.display="none";
 }
 jsbtn.onclick=function(){
     maincontent.style.display="none";
-    htmlCONTENT.style.display="none";
+    htmlCONTENT.style.display="block";
     csScontent.style.display="none";
-    jscontent.style.display="block";
+    jsContent.style.display="none";
+     about.style.display="none";
 }
+
+
